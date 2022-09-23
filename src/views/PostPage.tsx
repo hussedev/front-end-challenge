c7770064ea59f12f0a914714a5b50966bba6e268
Hussein Abdeljabbar Ismail Martinez <husse.dev@gmail.com>
2022-09-24 00:30:28 +0200

    PostPage edits posts

--- src/views/PostPage.tsx
+++ src/views/PostPage.tsx
@@ -1,5 +1,7 @@
+import { Button, Grid, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { getPost } from '../services';
+import { EditableTypography } from '../components/EditableTypography';
+import { getPost, patchPost } from '../services';
 
 interface PostPageProps {
   id: number;
@@ -8,21 +10,58 @@ interface PostPageProps {
 export const PostPage: React.FC<PostPageProps> = ({ id, goBack }) => {
   const [post, setPost] = useState<IPost>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [title, setTitle] = useState<string>('');
+  const [body, setBody] = useState<string>('');
+  const [disabled, setDisabled] = useState<boolean>(true);
 
   useEffect(() => {
     getPost(id).then((data) => {
       setPost(data);
+      setTitle(data.title);
+      setBody(data.body);
       setIsLoading(false);
     });
   }, []);
 
+  useEffect(() => {
+    if (title === post?.title && body === post?.body) setDisabled(false);
+    else setDisabled(true);
+  }, []);
+
+  const handleSubmit = async () => {
+    if (title !== post?.title || body !== post?.body) {
+      const newPost: IPostPatch = { title, body };
+      patchPost(id, newPost).then((status) => {
+        if (status === 200) alert('Post edited succesfully');
+      });
+    }
+  };
+
   return isLoading ? (
     <></>
   ) : (
-    <div onClick={() => goBack()}>
-      <p>{`id: ${post?.id} - user id: ${post?.userId}`}</p>
-      <p>{`title: ${post?.title}`}</p>
-      <p>{`body: ${post?.body}`}</p>
-    </div>
+    <Grid
+      container
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      sx={{ width: '100%', maxWidth: 720, margin: 'auto', bgcolor: 'background.paper' }}
+    >
+      <Grid item>
+        <Typography>{`id: ${post?.id} - user id: ${post?.userId}`}</Typography>
+      </Grid>
+      <Grid item>
+        <EditableTypography value={title} onChange={(value) => setTitle(value)} />
+      </Grid>
+      <Grid item>
+        <EditableTypography value={body} onChange={(value) => setBody(value)} />
+      </Grid>
+      <Grid item>
+        <Button onClick={goBack}>Return</Button>
+        <Button onClick={handleSubmit} disabled={disabled}>
+          Submit
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
