14ad290aa07d6d6ad03d93a0e57a0b9d78efc317
Hussein Abdeljabbar Ismail Martinez <husse.dev@gmail.com>
2022-09-24 00:29:50 +0200

    implemented fetchFilteredPosts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import { useEffect, useState } from 'react';
 import { PostList } from './views/PostList';
-import { getPosts } from './services';
+import { getPosts, getPostsByUser } from './services';
 import { PostPage } from './views/PostPage';
 
 const App = () => {
   const [posts, setPosts] = useState<IPost[]>([]);
   const [selectedPost, setSelectedPost] = useState<number | null>(null);
-
+  const [lastFilter, setLastFilter] = useState<string>('');
   useEffect(() => {
-    getPosts().then((data) => setPosts(data));
+    fetchPosts();
   }, []);
+
+  const fetchPosts = async () => {
+    getPosts().then((data) => setPosts(data));
+  };
+
+  const fetchFilteredPosts = async (id: number) => {
+    try {
+      getPostsByUser(id).then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleClick = (id: number) => {
     setSelectedPost(id);
   };
 
+  const handleFilter = async (id: string) => {
+    if (lastFilter !== id) {
+      if (id === '') {
+        await fetchPosts();
+      } else {
+        await fetchFilteredPosts(parseInt(id));
+      }
+      setLastFilter(id);
+    }
+  };
+
   return selectedPost ? (
     <PostPage id={selectedPost} goBack={() => setSelectedPost(null)} />
   ) : (
-    <PostList list={posts} onClick={handleClick} />
+    <PostList list={posts} onClick={handleClick} filter={handleFilter} />
   );
 };
 
