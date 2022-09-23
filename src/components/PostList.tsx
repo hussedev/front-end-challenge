2d7f6825c19e92dc69ff0aca6db3312314a3c35e
Hussein Abdeljabbar Ismail Martinez <husse.dev@gmail.com>
2022-09-23 18:54:31 +0200

    created PostList component

---
+++ src/components/PostList.tsx
@@ -0,0 +1,15 @@
+import { PostListItem } from './PostListItem';
+
+interface PostsListProps {
+  list: TPostArray;
+  onClick: (id: number) => void;
+}
+export const PostList: React.FC<PostsListProps> = ({ list, onClick }) => {
+  return (
+    <ul>
+      {list.map((post) => (
+        <PostListItem key={post.id} post={post} onClick={onClick} />
+      ))}
+    </ul>
+  );
+};
