2efe7d788b2eeb7e49a75005befb30e9b37a380b
Hussein Abdeljabbar Ismail Martinez <husse.dev@gmail.com>
2022-09-23 19:20:45 +0200

    App now renders the list or the selected post

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { getPost, getPosts } from './services';
-
-const MAX_BODY_SIZE = 20;
+import { useEffect, useState } from 'react';
+import { PostList } from './views/PostList';
+import { getPosts } from './services';
+import { PostPage } from './views/PostPage';
 
 const App = () => {
   const [posts, setPosts] = useState<IPost[]>([]);
-  const [selectedPost, setSelectedPost] = useState<IPost | null>(null);
+  const [selectedPost, setSelectedPost] = useState<number | null>(null);
 
   useEffect(() => {
     getPosts().then((data) => setPosts(data));
   }, []);
 
   const handleClick = (id: number) => {
-    getPost(id).then((data) => setSelectedPost(data));
+    setSelectedPost(id);
   };
 
-  const trimBody = (body: string) => (body.length > 20 ? `${body.slice(0, 20)}...` : body);
-
-  return (
-    <>
-      {selectedPost && (
-        <>
-          <h3>Selected Post</h3>
-          <p>{`id: ${selectedPost.id} - user id: ${selectedPost.userId}`}</p>
-          <p>{`title: ${selectedPost.title}`}</p>
-          <p>{`body: ${selectedPost.body}`}</p>
-        </>
-      )}
-      <h2>Post List</h2>
-      <ul>
-        {posts.map(({ id, userId, title, body }) => (
-          <li key={id} onClick={() => handleClick(id)}>
-            <p>{`id: ${id} - user id: ${userId}`}</p>
-            <p>{`title: ${title}`}</p>
-            <p>{`body: ${trimBody(body)}`}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+  return selectedPost ? (
+    <PostPage id={selectedPost} goBack={() => setSelectedPost(null)} />
+  ) : (
+    <PostList list={posts} onClick={handleClick} />
   );
 };
 
